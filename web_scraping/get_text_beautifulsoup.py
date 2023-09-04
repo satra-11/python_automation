3a9513631104b879f4848f6bf56dbf73d57b1f09
satra-11 <syuzuki546@gmail.com>
2023-09-04 15:49:59 +0900

    beautiful soupによるh1エレメントの取得

---
+++ web_scraping/get_text_beautifulsoup.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome()
+
+folder_path = os.path.dirname(__file__)
+print("プログラムファイルのディレクトリパス：", folder_path)
+
+url = "https://developer.mozilla.org/ja/docs/Web/CSS/::before"
+
+driver.get(url)
+time.sleep(5)
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+
+h1 = soup.find("h1")
+
+print(h1)
+print((h1.decode_contents(formatter="html")))
